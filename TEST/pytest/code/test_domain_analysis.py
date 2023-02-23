405a52f222cbf18e52675d5d925683e01daf1015
maruring <maruring3745@gmail.com>
2023-02-23 11:25:25 +0900

    pytest code change

--- TEST/pytest/code/test_domain_analysis.py
+++ TEST/pytest/code/test_domain_analysis.py
@@ -1,24 +1,24 @@
 from domain_analysis import DomainAnalysis
 
-def case1():
+def test_case1():
     assert DomainAnalysis(20,30).calc_total_score() == 50
 
-def case2():
+def test_case2():
     """self.aがon point, self.bがin point
     """
     assert DomainAnalysis(60,40).is_pass() == True
 
-def case3():
+def test_case3():
     """self.aがoff point, self.bがin point
     """
     assert DomainAnalysis(59,40).is_pass() == False
 
-def case4():
+def test_case4():
     """self.aがin point, self.bがon point
     """
     assert DomainAnalysis(70,20).is_pass() == True
 
-def case5():
+def test_case5():
     """self.aがin point, self.bがoff point
     """
     assert DomainAnalysis(70,19).is_pass() == False
